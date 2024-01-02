f52f92cf6d74c3d88e999bdfe441d1a7b0c923d0
Shiaba <>
2024-01-02 18:47:24 +0000

    Create posts app, add to models & serializers.py

---
+++ posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+"""
+Need to add categories to model & category options
+"""
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../cpsu6kqtsa5zauuo8set', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
